eadfedaed85906c6c70c12fbca4c8e060e81ad9b
Dmitriy Groschovskiy <groschovskiy@yahoo.com>
2015-06-18 22:09:24 +0300

    Update ViewController.m

--- Minsktrans/ViewController.m
+++ Minsktrans/ViewController.m
@@ -26,6 +26,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Authorization Method in Container
+
 - (IBAction)authWithCredentials:(id)sender {
     ControllerView *controllerEvent = [[ControllerView alloc] initWithNibName:@"ControllerView" bundle:nil];
     [self presentViewController:controllerEvent animated:YES completion:nil];
